22f6ff36799eed68d6d69a16f25f6e5696816e74
prudhvibodavula <prudhvikbodavula@gmail.com>
2025-08-25 21:53:13 -0700

    made changes in parameter inputs

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class Repo(Base):
     __tablename__ = "repos"
 
     id = Column(Integer, primary_key=True, index=True)
-    repo_name = Column(String, nullable=False)
-    commit_count = Column(Integer)
-    branch_count = Column(Integer)
-    last_commit_date = Column(DateTime)
+    repo_name = Column(String, nullable=False, unique=True)
+    commit_count = Column(Integer, default=0)
+    branch_count = Column(Integer, default=0)
+    last_commit_date = Column(DateTime, nullable=True)
